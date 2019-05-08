2b2e0cafe43396ba54fcd0365609958cb4a65f8b
Ola Podsiadlik <alecs@op.pl>
2019-05-08 14:14:28 +0200

    Add task 28.4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './store';
+// import DevTools from './DevTools';
+// import { getCountries } from './actions/actions-countries'
+// render(
+//     <Provider store={store}>
+//         <div>
+//         <h1>Inicjalizacja projektu</h1>
+//         <DevTools />
+//         </div>
+        
+//     </Provider>,
+//     document.getElementById('root')
+// );
+
+// store.dispatch(getCountries())
+
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries'
+import routes from './routes';
+import './country.css'
+
 render(
     <Provider store={store}>
-        <div>
-        <h1>Inicjalizacja projektu</h1>
-        <DevTools />
-        </div>
-        
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
-
-store.dispatch(getCountries())
